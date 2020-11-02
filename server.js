bcf9b05d0b5ccfa7071c343b616a6c6698b491f9
freshaluck <freshaluck@localhost.localdomain>
2020-11-02 06:30:43 -0600

    server

--- server.js
+++ server.js
@@ -26,7 +26,18 @@ app.use(express.json());
 app.use('/api', user);
 app.use('/api', form);
 
-const PORT = process.env.NODE_ENV || 5000
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+
+const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 })
